bda26b7b8f7fdb7a5cecef8c714719c15f44f6a7
Brady Koehler <me@bradykoehler.com>
2020-02-08 18:41:05 -0700

    Add destroy action to users controller

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -5,15 +5,7 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
-  test "should show user" do
-    get api_v1_user_url(@user), as: :json
-    assert_response :success
-
-    # Test to ensure response contains the correct email
-    json_response = JSON.parse(self.response.body)
-    assert_equal @user.email, json_response['email']
-  end
-
+  # CREATE
   test "should create user" do
     assert_difference('User.count') do
       post api_v1_users_url, params: { user: { email: 'test@bradykoehler.com', password: '123456' } }, as: :json
@@ -30,6 +22,17 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :unprocessable_entity
   end
 
+  # READ
+  test "should show user" do
+    get api_v1_user_url(@user), as: :json
+    assert_response :success
+
+    # Test to ensure response contains the correct email
+    json_response = JSON.parse(self.response.body)
+    assert_equal @user.email, json_response['email']
+  end
+
+  # UPDATE
   test "should update user" do
     patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } }, as: :json
     assert_response :success
@@ -38,5 +41,14 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   test "should not update user when invalid params are sent" do
     patch api_v1_user_url(@user), params: { user: { email: 'bad_email', password: '123456' } }, as: :json
     assert_response :unprocessable_entity
+  end
+
+  # DESTROY
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete api_v1_user_url(@user), as: :json
+    end
+
+    assert_response :no_content
   end
 end
