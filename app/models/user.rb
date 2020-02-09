8ace09b015e7aed25c1b98152a2d33d05422a3a8
Brady Koehler <me@bradykoehler.com>
2020-02-08 18:00:13 -0700

    Utilize bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
+
+  # Pass the hard work of password processing 
+  # off to bcrypt
+  has_secure_password
 end
